2c95f6011e53b37db538a23686ec483906e1fa16
Luke Harris <lukeharris@Lukes-MacBook-Pro.local>
2024-07-25 12:52:41 -0600

    ray lewis

--- src/client/src/api/axios.js
+++ src/client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: 'postgresql://clubhousehomie35_user:hSpbSCfrk2oFMmMZ6As2RKebl36ixoD2@dpg-cqggumiju9rs73ce8uu0-a.oregon-postgres.render.com/clubhousehomie35',
   headers: {
     'Content-Type': 'application/json',
   },
